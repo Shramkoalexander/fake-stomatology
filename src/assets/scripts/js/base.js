592f682c6ad75b716bd612e6b83e07a84da046ce
Alexander <shramkoalexander@gmail.com>
2020-03-09 22:55:32 +0500

    add form elements  to base js file

---
+++ src/assets/scripts/js/base.js
@@ -0,0 +1,8 @@
+export const elements = {
+   bookAppointment: document.querySelector('#book-appointment'),
+   firstNameInput: document.querySelector('#ba-fname'),
+   phoneInput: document.querySelector('#ba-phone'),
+   callbackTimeInput: document.querySelector('#ba-callback-time'),
+   officeAddressInput: document.querySelector('#ba-office-address'),
+   submitBtn: document.querySelector('#submit'),
+};
