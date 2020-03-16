93faf9da180c986ee864e74730880a51ad4dfe3c
Alexander <shramkoalexander@gmail.com>
2020-03-16 11:17:25 +0500

    change id for submit button

--- src/assets/scripts/js/base.js
+++ src/assets/scripts/js/base.js
@@ -2,7 +2,5 @@ export const elements = {
    bookAppointment: document.querySelector('#book-appointment'),
    firstNameInput: document.querySelector('#ba-fname'),
    phoneInput: document.querySelector('#ba-phone'),
-   callbackTimeInput: document.querySelector('#ba-callback-time'),
-   officeAddressInput: document.querySelector('#ba-office-address'),
-   submitBtn: document.querySelector('#submit'),
+   submitBtn: document.querySelector('#ba-submit'),
 };
