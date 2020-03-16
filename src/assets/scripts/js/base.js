509e3d2b63a995012feb63607bbc1ace8736e14b
Alexander <shramkoalexander@gmail.com>
2020-03-16 11:23:22 +0500

    change variables name for form fields in base js fille

--- src/assets/scripts/js/base.js
+++ src/assets/scripts/js/base.js
@@ -2,5 +2,7 @@ export const elements = {
    bookAppointment: document.querySelector('#book-appointment'),
    firstNameInput: document.querySelector('#ba-fname'),
    phoneInput: document.querySelector('#ba-phone'),
+   callbackTimeSelect: document.querySelector('#ba-callback-time'),
+   officeAddressSelect: document.querySelector('#ba-office-address'),
    submitBtn: document.querySelector('#ba-submit'),
 };
